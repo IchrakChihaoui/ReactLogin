d09241aee9f685696d4027a350a66cbb1662c5ee
IchrakChihaoui <ichrakchihaoui7@gmail.com>
2022-05-18 14:52:00 +0200

    auth

--- src/components/CusumButton/CustumButton.js
+++ src/components/CusumButton/CustumButton.js
@@ -1,20 +1,49 @@
-import { View, Text,Button,StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 
 
-const CustumButton = ({title}) => {
-  return (
-    <View  style={styles.btn} >
-        <Button  title={title}></Button>
-    </View>
-  )
+const CustumButton = ({ onPress, text, type = "PRIMARY", bgColor, fgColor }) => {
+    return (
+        <Pressable
+            onPress={onPress}
+            style={[
+            styles.btn, 
+            styles[`btn_${type}`],
+            bgColor ? { backgroundColor: bgColor } : {}
+            ]} >
+            <Text style={[
+            styles.text,
+            styles[`text_${type}`],
+            fgColor ? { color: fgColor } : {}
+            ]} >{text}</Text>
+        </Pressable>
+    )
 }
-const styles=StyleSheet.create({
-    btn:{
-        top:70,
-        paddingVertical:10
-        
+const styles = StyleSheet.create({
+    btn: {
+        width: '100%',
+        padding: 15,
+        marginVertical: 5,
+        alignItems: 'center',
+        borderRadius: 5,
+        top: 60
+    },
+    text: {
+        fontWeight: 'bold',
+        color: 'white',
+    },
+
+    btn_PRIMARY: {
+        backgroundColor: '#ff7d7d'
+    },
+
+    btn_TERITARY: {
+    },
+
+    text_TERITARY: {
+        color: 'gray'
     }
+
 })
 
 export default CustumButton
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -8,13 +8,41 @@ const SignInscreen = () => {
   const[username, setUsername]=useState('');
   const[password, setPassword]=useState('');
   const {height}=useWindowDimensions();
+  const onSignInPressed =()=>{
+    console.warn('sign in');
+    
+  };
+  const onforgetpasswordPressed=()=>{
+    console.warn('forget password')
+  }
+  const signInWidhFacebook=()=>{
+    console.warn('sign in with facebook')
+
+  };
+  const signInWidhGoogle=()=>{
+    console.warn('sign in with google')
+
+  };
+  const signInWidhApple=()=>{
+    console.warn('sign in with Apple')
+
+  };
+
+
+
   return (
     <View style={styles.root}>
   <Image source={logo} style={[styles .logo , {height:height*0.3}]} resizeMode='contain'/>
   <CustumInputs placeholder="Username" value={username} setValue={setUsername}/>
   <CustumInputs placeholder="password" value={password} setValue={setPassword} secureTextEntry={true}/>
-  <CusumButton title='Login'></CusumButton>
-  <CusumButton title='Register'></CusumButton>
+  <CusumButton  text="sign in " onPress={onSignInPressed} />
+  <CusumButton  text="forget password " onPress={onforgetpasswordPressed} type="TERITARY"/>
+  <CusumButton  bgColor="#E7EAF4" fgColor="#4765A9"  text="sign in widh facebook " onPress={signInWidhFacebook} />
+  <CusumButton  bgColor='#fae9eA' fgColor='#dd4d44' text="sign in widh google " onPress={signInWidhGoogle} />
+  <CusumButton   bgColor='#e3e3e3' fgColor='#363636' text="sign in widh Apple " onPress={signInWidhApple} />
+  
+ 
+  
 
     </View>
   )
